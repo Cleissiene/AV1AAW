122bfa27b01a4cb6e7ea1b01d37b78f777776dfa
11513288 <@123>
2024-04-26 20:46:27 -0300

    Feat: cria Api de Noticia

--- src/main/java/com/example/Noticia/controller/NoticiaController.java
+++ src/main/java/com/example/Noticia/controller/NoticiaController.java
@@ -1,11 +1,9 @@
 package com.example.Noticia.controller;
 
-import com.example.Noticia.model.NoticiaEntity;
 import com.example.Noticia.service.NoticiaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/noticia")
