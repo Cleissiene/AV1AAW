b08241d005bb14afe36d8e9b430b42103e7319ec
11513288 <@123>
2024-04-26 20:43:41 -0300

    Feat: cria Api de Noticia

--- src/main/java/com/example/Noticia/repository/NoticiaRepository.java
+++ src/main/java/com/example/Noticia/repository/NoticiaRepository.java
@@ -3,4 +3,6 @@ package com.example.Noticia.repository;
 import com.example.Noticia.model.NoticiaEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface NoticiaRepository extends MongoRepository
+public interface NoticiaRepository extends MongoRepository<NoticiaEntity, String>{
+
+}
